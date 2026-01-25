85cbec583b4b81ffd7af782e56c32ee147d71337
Leonidas Constantinou <leonidasc94.dev@gmail.com>
2026-01-25 23:32:47 +0200

    Movement logic

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
     
     private PlayerInputSet input;
     private StateMachine stateMachine;
@@ -13,9 +14,13 @@ public class Player : MonoBehaviour
 
     public Vector2 moveInput { get; private set; }
     
+    [Header("Movement Details")]
+    public float moveSpeed;
+    
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         
         stateMachine = new StateMachine();
         input = new PlayerInputSet();
@@ -45,5 +50,10 @@ public class Player : MonoBehaviour
     private void Update()
     {
         stateMachine.UpdateActiveState();
+    }
+
+    public void SetVelocity(float xVelocity, float yVelocity)
+    {
+        rb.linearVelocity = new Vector2(xVelocity, yVelocity);
     }
 }
--- Assets/Player_MoveState.cs
+++ Assets/Player_MoveState.cs
@@ -11,5 +11,7 @@ public class Player_MoveState : EntityState
         base.Update();
         if (player.moveInput.x == 0)
             stateMachine.ChangeState(player.idleState);
+        
+        player.SetVelocity(player.moveInput.x * player.moveSpeed, rb.linearVelocity.y);
     }
 }
